de44aa49191ecf62f2d18ed272395299da6018b6
Jeremy Nevill <jeremy@nevill.net>
2014-03-19 22:55:46 +0000

    First foray into layout@

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,12 +4,11 @@ angular.module('fireworkNgApp')
     .controller('MainCtrl', function ($scope) {
 
         $scope.fireworkEvents = [
-            {message: 'jeremy ate 2 lunches'},
-            {message: 'jeremy ate 3 breakfasts'},
-            {message: 'jeremy ran 3 miles'},
+            {message: '@jeremy ate 2lunches 01/jan/2012', actor: 'jeremy', action: 'ate', amount: 2, units: 'lunches', date: '01/jan/2012'},
+            {message: '@jeremy ate 3breakfasts 01/jan/2012', actor: 'jeremy', action: 'ate', amount: 2, units: 'lunches', date: '01/jan/2012'},
+            {message: '@fred ran 3miles 01/jan/2012', actor: 'fredd', action: 'ran', amount: 3, units: 'miles', date: '01/jan/2012'},
         ];
 
-        $scope.cheese = "Cheddar";
 
     });
 
