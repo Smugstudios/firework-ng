2e1a03ca65dc44cf77a1a326db469f407dabfd9a
Jeremy Nevill <jeremy@nevill.net>
2014-03-20 23:06:32 +0000

    Add form added and delete function wired in.@

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,8 +7,14 @@ angular.module('fireworkNgApp')
             {message: '@jeremy ate 2lunches 01/jan/2012', actor: 'jeremy', action: 'ate', amount: 2, units: 'lunches', date: '01/jan/2012'},
             {message: '@jeremy ate 3breakfasts 01/jan/2012', actor: 'jeremy', action: 'ate', amount: 2, units: 'lunches', date: '01/jan/2012'},
             {message: '@fred ran 3miles 01/jan/2012', actor: 'fredd', action: 'ran', amount: 3, units: 'miles', date: '01/jan/2012'},
+            {message: '@fred ran 3miles 01/jan/2012', actor: 'fredd', action: 'ran', amount: 3, units: 'miles', date: '01/jan/2012'},
+            {message: '@fred ran 3miles 01/jan/2012', actor: 'fredd', action: 'ran', amount: 3, units: 'miles', date: '01/jan/2012'},
+            {message: '@fred ran 3miles 01/jan/2012', actor: 'fredd', action: 'ran', amount: 3, units: 'miles', date: '01/jan/2012'}
         ];
 
+        $scope.delete = function (idx) {
+            $scope.fireworkEvents.splice(idx, 1);
+        };
 
     });
 
